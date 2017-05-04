4295647a25141347a9c252d56f74dbc01786187b
Jacob Lindstrom <lindstromj@spu.edu>
2017-05-03 19:32:09 -0700

    Put request working for like and dislike

--- aktve/www/js/app/swipe.page.js
+++ aktve/www/js/app/swipe.page.js
@@ -5,6 +5,7 @@ class SwipePage {
         // Create some events to process user interaction
         // (TODO: Add in touch/swipe events that call these same functions. jQuery
         // makes this very easy.)
+        // TODO: Buttons are switched. Image doesnt correspond to correct one. 
         $("#SwipeLikeButton").click(swipe_page.handleLike);
         $("#SwipeDislikeButton").click(swipe_page.handleDislike);
     }
@@ -12,11 +13,36 @@ class SwipePage {
     // handleLike() is a handler for the event of liking another user.
     handleLike() {
         $("#SwipePicture").attr('src', 'http://hdwallpapershdpics.com/wp-content/uploads/2016/08/V4Doic.jpg');
+
+        $.ajax({
+            type: 'put',
+            url: "https://api.aktve-app.com/users/" + 1 + "/feeling?token=" + APITestToken, //Change to actual facebook token
+            dataType: 'json',
+            data: { 'feeling': "like" },
+            context: this, // Make the callaback function's `this` variable point to this User object
+            success: function (data) {
+                console.log(data.Success.success);
+                console.log(data.Success.error);
+            }
+        });
     }
 
     // handleDislike() is a handler for the event of disliking another user.
     handleDislike() {
         $("#SwipePicture").attr('src', 'https://pbs.twimg.com/profile_images/483129073208528896/3_wpD7SL.jpeg');
+
+        $.ajax({
+            type: 'put',
+            url: "https://api.aktve-app.com/users/" + 1 + "/feeling?token=" + APITestToken, //Change to actual facebook token
+            dataType: 'json',
+            data: { 'feeling': "dislike" },
+            context: this, // Make the callaback function's `this` variable point to this User object
+            success: function (data) {
+                console.log(data.Success.success);
+                console.log(data.Success.error);
+            }
+        });
+
     }
 }
 
