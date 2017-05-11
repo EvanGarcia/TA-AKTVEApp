c455c4b71da673515f40b5d71a50fcb2fecd8c6b
Jacob Lindstrom <lindstromj@spu.edu>
2017-05-11 09:55:12 -0700

    Swipe Page Fully Working
    Only issue is with the scoping problem that prevents everything from
    being put into a function.

--- aktve/www/js/app/swipe.page.js
+++ aktve/www/js/app/swipe.page.js
@@ -1,6 +1,6 @@
 // SwipePage is the class representing the model and implementing the controller
 // functions for the "swipe" page.
-
+// DUE TO SCOPING issues, the code inside each handle function is duplicated - Inside handlelike and handledislike cannot access outside  
 class SwipePage {
 
     Init() {
@@ -8,7 +8,6 @@ class SwipePage {
         // (TODO: Add in touch/swipe events that call these same functions. jQuery
         // makes this very easy.)
 
-        // TODO: Buttons are switched. Image doesnt correspond to correct one. 
         $("#SwipeLikeButton").click(swipe_page.handleLike);
         $("#SwipeDislikeButton").click(swipe_page.handleDislike);
         
@@ -19,9 +18,7 @@ class SwipePage {
             dataType: 'json',
             context: this, // Make the callaback function's `this` variable point to this User object
             success: function (data) {
-                potentials = data.Data.potential_user_ids;
-               
-
+                potentials = data.Data.potential_user_ids;    
             },
             async: false
         });
@@ -34,20 +31,53 @@ class SwipePage {
             dataType: 'json',
             context: this, // Make the callaback function's `this` variable point to this User object
             success: function (data) {
-                console.log(data.Success.success);
-                console.log(data.Success.error);
-                console.log(data.Data.user.name)
-                $("#userName").html(data.Data.user.name);
+                if (data.Success.success == false) {
+                    document.getElementById("cardContent").
+                        innerHTML = "<div class=\"card-header\">There are no more people in your area!</div><div class=\"card-content\"><div class=\"card-content-inner\">Please check back later for more new potential friends! Have fun exploring!</div>";
+                    $("#SwipeLikeButton").hide();
+                    $("#SwipeDislikeButton").hide();
+                } else {
+                    $("#SwipeLikeButton").show();
+                    $("#SwipeDislikeButton").show();
+                    document.getElementById("profPic").
+                        innerHTML = "<img data-src=\""+ data.Data.user.images[0]+"\"id=\"SwipePicture\" class=\"lazy lazy-fadeIn app-swipe-picture\">";
+                    $("#userName").html(data.Data.user.name + ", " + data.Data.user.age);
+                    $("#lastAct").html("Last Active: " + new Date(data.Data.user.last_active));
+                    $("#SwipeInterests").html("");
+
+                    $.each(data.Data, function (key, value) { // First Level
+                        $.each(value.interests, function (k, v) {  // The contents inside stars
+                            document.getElementById("SwipeInterests").
+                        innerHTML += "<div class=\"chip\">\n<div class=\"chip-media bg-gray\">" + v + "</div>\n<div class=\"chip-label\">" + k + "</div></div>";
+                        });
+                    });
+                    $("#SwipeDetails").html("");
+
+
+                    for (var i = 0; i < data.Data.user.tags.length; i++) {
+                        if (data.Data.user.tags[i] == "friends_men") {
+                            document.getElementById("SwipeDetails").
+                        innerHTML += "<div class=\"chip bg-blue\">\n<div class=\"chip-media\"><i class=\"fa fa-hand-peace-o\" aria-hidden=\"true\"></i></div><div class=\"chip-label\">Men</div></div>"
+                        } else if (data.Data.user.tags[i] == "friends_women") {
+                            document.getElementById("SwipeDetails").
+                        innerHTML += "<div class=\"chip bg-blue\">\n<div class=\"chip-media\"><i class=\"fa fa-hand-peace-o\" aria-hidden=\"true\"></i></div><div class=\"chip-label\">Women</div></div>"
+                        } else if (data.Data.user.tags[i] == "dates_women") {
+                            document.getElementById("SwipeDetails").
+                        innerHTML += "<div class=\"chip bg-red\">\n<div class=\"chip-media\"><i class=\"fa fa-heart-o\" aria-hidden=\"true\"></i></div><div class=\"chip-label\">Women</div></div>"
+                        } else if (data.Data.user.tags[i] == "dates_men") {
+                            document.getElementById("SwipeDetails").
+                        innerHTML += "<div class=\"chip bg-red\">\n<div class=\"chip-media\"><i class=\"fa fa-heart-o\" aria-hidden=\"true\"></i></div><div class=\"chip-label\">Men</div></div>"
+                        }
+                    }
+
+                }
             }
         });
-
-
     }
+
 
     // handleLike() is a handler for the event of liking another user.
     handleLike() {
-        $("#SwipePicture").attr('src', 'http://hdwallpapershdpics.com/wp-content/uploads/2016/08/V4Doic.jpg');
-
         pontentialIndex++;
 
         $.ajax({
@@ -68,15 +98,14 @@ class SwipePage {
             dataType: 'json',
             context: this, // Make the callaback function's `this` variable point to this User object
             success: function (data) {
-                console.log(data.Success.success);
                 if (data.Success.success == false) {
                     document.getElementById("cardContent").
                         innerHTML = "<div class=\"card-header\">There are no more people in your area!</div><div class=\"card-content\"><div class=\"card-content-inner\">Please check back later for more new potential friends! Have fun exploring!</div>";
                     $("#SwipeLikeButton").hide();
                     $("#SwipeDislikeButton").hide();
                 } else {
-                    console.log(data.Success.error);
-                    console.log(data.Data.user.name)
+                    document.getElementById("profPic").
+                        innerHTML = "<img data-src=\"" + data.Data.user.images[0] + "\"id=\"SwipePicture\" class=\"lazy lazy-fadeIn app-swipe-picture\">";
                     $("#userName").html(data.Data.user.name + ", " + data.Data.user.age);
                     $("#lastAct").html("Last Active: " + new Date(data.Data.user.last_active));
                     $("#SwipeInterests").html("");
@@ -88,7 +117,6 @@ class SwipePage {
                         });
                     });
                     $("#SwipeDetails").html("");
-
 
                     for (var i = 0; i < data.Data.user.tags.length; i++)
                     {
@@ -114,21 +142,10 @@ class SwipePage {
                 }
             }
         });
-
     }
-
-    /*
-    <div class="chip">
-    <div class="chip-media bg-gray">2</div>
-    <div class="chip-label">Skiing</div>
-    </div>*/
-
-
 
     // handleDislike() is a handler for the event of disliking another user.
     handleDislike() {
-        $("#SwipePicture").attr('src', 'https://pbs.twimg.com/profile_images/483129073208528896/3_wpD7SL.jpeg');
-
         pontentialIndex++;
 
         $.ajax({
@@ -149,14 +166,46 @@ class SwipePage {
             dataType: 'json',
             context: this, // Make the callaback function's `this` variable point to this User object
             success: function (data) {
-                console.log(data.Success.success);
-                console.log(data.Success.error);
-                console.log(data.Data.user.name)
-                $("#userName").html(data.Data.user.name);
+                if (data.Success.success == false) {
+                    document.getElementById("cardContent").
+                        innerHTML = "<div class=\"card-header\">There are no more people in your area!</div><div class=\"card-content\"><div class=\"card-content-inner\">Please check back later for more new potential friends! Have fun exploring!</div>";
+                    $("#SwipeLikeButton").hide();
+                    $("#SwipeDislikeButton").hide();
+                } else {
+                    document.getElementById("profPic").
+                        innerHTML = "<img data-src=\"" + data.Data.user.images[0] + "\"id=\"SwipePicture\" class=\"lazy lazy-fadeIn app-swipe-picture\">";
+                    $("#userName").html(data.Data.user.name + ", " + data.Data.user.age);
+                    $("#lastAct").html("Last Active: " + new Date(data.Data.user.last_active));
+                    $("#SwipeInterests").html("");
+
+                    $.each(data.Data, function (key, value) { // First Level
+                        $.each(value.interests, function (k, v) {  // The contents inside stars
+                            document.getElementById("SwipeInterests").
+                        innerHTML += "<div class=\"chip\">\n<div class=\"chip-media bg-gray\">" + v + "</div>\n<div class=\"chip-label\">" + k + "</div></div>";
+                        });
+                    });
+                    $("#SwipeDetails").html("");
+
+
+                    for (var i = 0; i < data.Data.user.tags.length; i++) {
+                        if (data.Data.user.tags[i] == "friends_men") {
+                            document.getElementById("SwipeDetails").
+                        innerHTML += "<div class=\"chip bg-blue\">\n<div class=\"chip-media\"><i class=\"fa fa-hand-peace-o\" aria-hidden=\"true\"></i></div><div class=\"chip-label\">Men</div></div>"
+                        } else if (data.Data.user.tags[i] == "friends_women") {
+                            document.getElementById("SwipeDetails").
+                        innerHTML += "<div class=\"chip bg-blue\">\n<div class=\"chip-media\"><i class=\"fa fa-hand-peace-o\" aria-hidden=\"true\"></i></div><div class=\"chip-label\">Women</div></div>"
+                        } else if (data.Data.user.tags[i] == "dates_women") {
+                            document.getElementById("SwipeDetails").
+                        innerHTML += "<div class=\"chip bg-red\">\n<div class=\"chip-media\"><i class=\"fa fa-heart-o\" aria-hidden=\"true\"></i></div><div class=\"chip-label\">Women</div></div>"
+                        } else if (data.Data.user.tags[i] == "dates_men") {
+                            document.getElementById("SwipeDetails").
+                        innerHTML += "<div class=\"chip bg-red\">\n<div class=\"chip-media\"><i class=\"fa fa-heart-o\" aria-hidden=\"true\"></i></div><div class=\"chip-label\">Men</div></div>"
+                        }
+                    }
+
+                }
             }
         });
-
-
     }
 }
 
