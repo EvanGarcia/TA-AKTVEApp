b756a8b47311315417c122b5304feed40dfdd389
Jacob Lindstrom <lindstromj@spu.edu>
2017-05-27 18:37:59 -0700

    Settings Page

---
+++ aktve/www/js/app/settings.page.js
@@ -0,0 +1,61 @@
+﻿// Settings page
+class SettingsPage {
+    constructor() {
+        
+    }
+
+
+    Init() {
+        $("#settingsback").click(settings_page.storeSettings);
+        $("#addInterest").click(settings_page.addInterest);
+    } 
+
+    storeSettings() {
+
+        var locisChecked = $("#locationsharing").prop("checked");
+        console.log(locisChecked);
+        var menisChecked = $("#menbox").prop("checked");
+        console.log(menisChecked);
+        var womenisChecked = $("#womenbox").prop("checked");
+        console.log(womenisChecked);
+        var mendateisChecked = $("#mendatebox").prop("checked");
+        console.log(mendateisChecked);
+        var womendateisChecked = $("#womendatebox").prop("checked");
+        console.log(womendateisChecked);
+
+            $.ajax({
+                type: 'post',
+                url: "https://api.aktve-app.com/me/settings?token=" + APIUserToken, //Change to actual facebook token
+                dataType: 'json',
+                data: {
+                    'sharelocation': locisChecked,
+                    'friendmen': menisChecked,
+                    'friendwomen': womendateisChecked,
+                    'datemen': mendateisChecked,
+                    'datewomen': womendateisChecked
+                },
+                context: this, // Make the callaback function's `this` variable point to this User object
+                success: function (data) {
+                    console.log(data.Success.success);
+                    console.log(data.Success.error);
+                }
+            });
+          
+    }
+
+    addInterest() {
+        interArr.push($("#activityName").find("option:selected").text());
+        skillArr.push($("#activityLevel").find("option:selected").text());
+    }
+}
+
+// Instantiate a model/controller for the page
+let settings_page = new SettingsPage();
+let interArr = [];
+let skillArr = [];
+// Perform necessary steps once the page is loaded.
+myApp.onPageInit('settings', function (page) {
+    console.log("we here");
+    settings_page.Init();
+
+});
